423e58816dcf63c0c531edf1d75931f47f85af87
Ladislav Vondracek <lawondyss@gmail.com>
2014-11-22 08:05:02 +0100

    Added installer for Composer

---
+++ src/Canterville/Installer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class Installer
+ * @package Canterville
+ * @author Ladislav Vondráček
+ */
+
+namespace Canterville;
+
+use Canterville\Installers as CI;
+use Composer\Script\Event;
+
+
+class Installer
+{
+  /**
+   * Installation all required libraries
+   */
+  public static function install(Event $event)
+  {
+    $composer = $event->getComposer();
+
+    $phantom = new CI\PhantomInstaller;
+    $phantom->install($composer);
+
+    $casper = new CI\CasperInstaller;
+    $casper->install($composer);
+  }
+}
